6531a45b7389726bcf3064815097520cd4c4ccfa
Kim <whitney.k@husky.neu.edu>
2017-12-01 16:06:00 -0500

    Add the react files

--- inspyro_web/client.py
+++ inspyro_web/client.py
@@ -1,19 +1,19 @@
-# client.py  
+# client.py
 import socket
 
 # create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # get local machine name
-host = socket.gethostname()                           
+host = socket.gethostname()
 
-port = input('Port: ') 
+port = 40295
 
 # connection to hostname on the port.
-s.connect(('10.42.0.20', port))                               
+s.connect(('192.168.2.2', port))
 while True:
-# Receive no more than 1024 bytes
-  tm = s.recv(1024)                                 
-  print tm.decode('utf-8')
-s.close()
+  # Receive no more than 1024 bytes
+    tm = s.recv(1024)
+    print(tm.decode('utf-8'))
 
+s.close()
--- inspyro_web/static/js/App.jsx
+++ inspyro_web/static/js/App.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import axios from 'axios';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {'time': Date.now(),
+        'probability': 0}
+  }
+
+  componentDidMount() {
+    // this.interval = setInterval(() => this.setState({'time': Date.now()}), 1000);
+    this.interval = setInterval(this.timer.bind(this), 1000)
+  }
+
+  timer() {
+      axios.get('/api/v1.0/probability')
+          .then(res => {
+              var r = JSON.parse(JSON.stringify(res))
+              console.log(r)
+              // console.log(JSON.parse(res))
+              console.log(JSON.parse(JSON.stringify(res)))
+              this.setState({'time': Date.now(), 'probability': r.data.probability})
+      })
+      .catch(er => {console.log(er)})
+
+  }
+
+  componentWillUnmount() {
+      clearInterval(this.interval);
+  }
+
   render () {
-    return <p> Hello React!</p>;
+      console.log(this.state)
+    return <p>hello {this.state.probability}</p>;
   }
 }
